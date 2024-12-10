1aab743781fd30ab8ed19b3b3601ab656bdb2d33
Zegaroth <jonpatrickheffley@gmail.com>
2024-12-09 22:33:28 -0600

    multiplayer revamp part 1
    used Battery Acid Dev's tutorial. movement and retina and camera and animations are all synced. next: projectiles, player rightclick navigation, enemy spawn and navigation

---
+++ scripts/input_synchronizer.gd
@@ -0,0 +1,62 @@
+extends MultiplayerSynchronizer
+
+@onready var player = $".."
+
+var isUp
+var isDown
+var isLeft
+var isRight
+var rightClicked
+var esc
+
+@export var angle := float(0)
+@export var mouse_rel = Vector2.ZERO
+@export var mouse_pos = Vector2.ZERO
+var screen = Vector2.ZERO  
+
+func _ready():
+	if get_multiplayer_authority() != multiplayer.get_unique_id():
+		set_process(false)
+		set_physics_process(false)
+	
+	screen = get_viewport().get_visible_rect().size
+	screen = screen / 2
+	
+	isUp = Input.is_action_pressed("up")
+	isDown = Input.is_action_pressed("down")
+	isLeft = Input.is_action_pressed("left")
+	isRight = Input.is_action_pressed("right")
+	rightClicked = Input.is_action_just_pressed("rightclick")
+	esc = Input.is_action_pressed("esc")
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouse_pos = Vector2(event.position)
+		mouse_rel = mouse_pos - screen
+		angle = atan2(mouse_rel.y, mouse_rel.x) # returns an angle
+
+func _physics_process(delta: float) -> void:
+	isUp = Input.is_action_pressed("up")
+	isDown = Input.is_action_pressed("down")
+	isLeft = Input.is_action_pressed("left")
+	isRight = Input.is_action_pressed("right")
+	rightClicked = Input.is_action_just_pressed("rightclick")
+	esc = Input.is_action_pressed("esc")
+	if esc:
+		quitgame()
+	
+
+func quitgame():
+	if multiplayer.is_server():
+		multiplayer.peer_disconnected.connect(delete_player)
+		delete_player(player.player_id)
+
+func delete_player(id):
+	_delete_player.rpc(id)
+	
+@rpc("any_peer", "call_local")
+func _delete_player(id):
+	get_node(str(id)).queue_free()
+
+func _process(delta):
+	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,9 +5,12 @@ class_name Player
 @export var player_id := 1:
 	set(id):
 		player_id = id
+		$InputSynchronizer.set_multiplayer_authority(id)
 
 @onready var health_system: Node2D = $HealthSystem as HealthSystem
 @onready var player_ui: CanvasLayer = $PlayerUI as PlayerUI
+@onready var input_synchronizer: MultiplayerSynchronizer = $InputSynchronizer
+
 
 @export var speed: int = 300 #used for reticle
 @export var rightclickaccel: int = 7
@@ -16,35 +19,81 @@ class_name Player
 @export var seconds_between_shots: float = .3
 @export var spread: float = .025
 
-var movement_dir: Vector2 = Vector2.ZERO 
-var angle
+@export var movement_dir: Vector2 = Vector2.ZERO 
+@export var angle = (global_position).angle()
 @onready var retina: Sprite2D = $Retina
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-var wasd_moving = false
-var rightclick_moving = false
-var walking = false
-var rightclickpos
+@export var wasd_moving = false
+@export var rightclick_moving = false
+@export var walking = false
+@export var rightclickpos = global_position
 @onready var nav: NavigationAgent2D = $NavigationAgent2D
-var h_scale = true
+@export var h_scale = true
 
 @onready var camera_2d: Camera2D = $Camera2D
 
-func _enter_tree() -> void:
-	set_multiplayer_authority(name.to_int())
+#func _enter_tree() -> void:
+	#set_multiplayer_authority(name.to_int())
 
 func _ready():
 	player_ui.set_life_bar_max_value(health_system.base_health)
 	player_ui.update_life_bar_value(health_system.current_health)
-	if is_multiplayer_authority():
+	if multiplayer.get_unique_id() == player_id:
 		$PlayerUI.visible = true
 		$Retina.visible = true
 		camera_2d.make_current()
+	else:
+		camera_2d.enabled = false
 
-#@rpc("any_peer", "call_local", "reliable")
-func _physics_process(delta: float) -> void:
-	if !is_multiplayer_authority():
-		return
+func _apply_animations(delta):
+	if walking:
+		animated_sprite_2d.play("walk")
+	else:
+		animated_sprite_2d.play("default")
+	if (input_synchronizer.mouse_rel.x > global_position.x) and !h_scale:
+		self.transform.x *= -1.0
+		h_scale=true
+	elif (input_synchronizer.mouse_rel.x < global_position.x) and h_scale:
+		self.transform.x *= -1.0
+		h_scale=false
+	
+func _apply_movement_from_input(delta):
+	#if !is_multiplayer_authority():
+	#	return
+	movement_dir = Vector2.ZERO
+	walking = false
+	if $InputSynchronizer.rightClicked:
+		wasd_moving = false
+		rightclick_moving = true
+		rightclickpos = input_synchronizer.mouse_rel
+	
+	if $InputSynchronizer.isUp:
+		wasd_moving = true
+		rightclick_moving = false
+		movement_dir = movement_dir + Vector2.UP
+		walking = true
+	if $InputSynchronizer.isDown:
+		wasd_moving = true
+		rightclick_moving = false
+		movement_dir = movement_dir + Vector2.DOWN
+		walking = true
+	if $InputSynchronizer.isLeft:
+		wasd_moving = true
+		rightclick_moving = false
+		movement_dir = movement_dir + Vector2.LEFT
+		walking = true
+	if $InputSynchronizer.isRight:
+		wasd_moving = true
+		rightclick_moving = false
+		movement_dir = movement_dir + Vector2.RIGHT
+		walking = true
+	#if $InputSynchronizer.esc:
+	#	$"../".exit_game(name.to_int())
+	#	get_tree().quit()
+	
+	angle = input_synchronizer.angle
+	
 	#set up nav for right click
 	if rightclick_moving:
 		var navdir = Vector2()
@@ -62,61 +111,18 @@ func _physics_process(delta: float) -> void:
 		velocity = movement_dir * speed
 	#angle isn't decided by move mode
 	move_and_slide()
-	angle = (get_global_mouse_position() - global_position).angle()
+	angle = input_synchronizer.angle
 	if angle:
 		retina.global_rotation = angle
-		#below is used for ships
-		#global_rotation = lerp_angle(global_rotation, angle, delta * rot_speed)
-	if walking:
-		animated_sprite_2d.play("walk")
-	else:
-		animated_sprite_2d.play("default")
-	if (get_global_mouse_position().x > global_position.x) and !h_scale:
-		self.transform.x *= -1.0
-		h_scale=true
-	elif (get_global_mouse_position().x < global_position.x) and h_scale:
-		self.transform.x *= -1.0
-		h_scale=false
-	#if str(self.transform.x.x) == "-0": self.transform.x.x=0
-	#if str(self.transform.x.y) == "-0": self.transform.x.y=0
-	#if str(self.transform.y.x) == "-0": self.transform.y.x=0
-	#if str(self.transform.y.y) == "-0": self.transform.y.y=0
-	
+
+#@rpc("any_peer", "call_local", "reliable")
+func _physics_process(delta: float) -> void:
+	if multiplayer.is_server():
+		_apply_movement_from_input(delta)
+	if not multiplayer.is_server() || MainMenu.hosting:
+		_apply_animations(delta)
 func _input(event):
-	if !is_multiplayer_authority():
-		return
-	movement_dir = Vector2.ZERO
-	walking = false
-	if Input.is_action_just_pressed("rightclick"):
-		wasd_moving = false
-		rightclick_moving = true
-		rightclickpos = get_global_mouse_position()
-	
-	if Input.is_action_pressed("up"):
-		wasd_moving = true
-		rightclick_moving = false
-		movement_dir = movement_dir + Vector2.UP
-		walking = true
-	if Input.is_action_pressed("down"):
-		wasd_moving = true
-		rightclick_moving = false
-		movement_dir = movement_dir + Vector2.DOWN
-		walking = true
-	if Input.is_action_pressed("left"):
-		wasd_moving = true
-		rightclick_moving = false
-		movement_dir = movement_dir + Vector2.LEFT
-		walking = true
-	if Input.is_action_pressed("right"):
-		wasd_moving = true
-		rightclick_moving = false
-		movement_dir = movement_dir + Vector2.RIGHT
-		walking = true
-	if Input.is_action_pressed("esc"):
-		$"../".exit_game(name.to_int())
-		get_tree().quit()
-	
-	angle = (get_global_mouse_position() - global_position).angle()
+	pass
 
 #@rpc("any_peer", "call_local", "reliable")
 func take_damage(damage: int):
